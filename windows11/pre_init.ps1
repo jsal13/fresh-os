f1998ee1c31e516e60ed2d8bb61358b2da50fd09
James Salvatore <jimmy.c.salvatore@gmail.com>
2024-07-26 02:06:03 -0500

    Update pre_init.ps1

--- windows11/pre_init.ps1
+++ windows11/pre_init.ps1
@@ -1,16 +1,16 @@
-# ! Run as Admin, and run the following command first:
-# Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+# ! Run as Admin, and run the following command first.
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 
-# ! Get the new powershell: 
-# https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.4#msi
-
+# Upgrade Winget.
+# See also: https://github.com/microsoft/winget-cli/issues/3832
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile winget.msixbundle
+Add-AppPackage -ForceApplicationShutdown .\winget.msixbundle
+del .\winget.msixbundle
 
 # Get the repo and initialize the fresh-os stuff.
 # YOU MAY NEED TO CLOSE AND RE-OPEN POWERSHELL.
 Invoke-WebRequest -Uri https://github.com/jsal13/fresh-os/archive/refs/heads/main.zip -OutFile ~/Downloads/main.zip
 Expand-Archive ~\Downloads\main.zip -DestinationPath ~\Downloads\main
-cd ~/Downloads/main/fresh-os-main/windows11
-./init.ps1
 
 # Update Powershell
 winget install Microsoft.PowerShell
@@ -18,6 +18,8 @@ winget install Microsoft.PowerShell
 # Install WSL
 wsl --install
 
-# Update Windows
-Install-Module -AcceptLicense -Force PSWindowsUpdate
-Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+# Update Windows (Note: these don't seem to work as well, but Win Update is fine manually.)
+# Install-Module -AcceptLicense -Force PSWindowsUpdate
+# Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+
+# !! Now run the init.ps1 file commands.
